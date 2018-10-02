29fd82438425644890e6a41c70362092d04b3aff
Szymon Nowicki <sznowicki@eskalacja.com>
2018-10-02 12:03:18 +0200

    Added github docs question. Added CHANGELOG.md question.

---
+++ generators/docs/index.js
@@ -0,0 +1,38 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+/**
+ * Generates Shopgate required GitHub docs.
+ *
+ * @type {module.exports}
+ */
+module.exports = class extends Generator {
+  prompting() {
+    this.props = {};
+
+    return this.prompt([
+      {
+        type: 'confirm',
+        name: 'generateDocs',
+        message: 'Do you want to generate extra docs (e.x. CONTRIBUTING.md)?',
+        default: true
+      }
+    ]).then(props => {
+      this.props.generateDocs = props.generateDocs;
+    });
+  }
+
+  writing() {
+    const extensionPath = this.destinationPath(
+      `extensions/${this.options.config.extension.organization}-${
+        this.options.config.extension.name
+      }/`
+    );
+
+    if (!this.props.generateDocs) {
+      this.fs.delete(extensionPath + 'docs/CONTRIBUTING.md');
+      this.fs.delete(extensionPath + 'docs/PULL_REQUEST_TEMPLATE.md');
+      this.fs.delete(extensionPath + 'docs/CODE_OF_CONDUCT.md');
+    }
+  }
+};
